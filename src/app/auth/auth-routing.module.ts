80d3c87182757c548dc37868cd31966e36e8121b
Ankuse <esukna@gmail.com>
2017-11-30 10:05:23 +0300

    update package json change true command for dev command -- correctly npm dev --- End rgistration and login component and module

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AuthComponent } from './auth.component';
+
+const routes: Routes = [
+  {path: '', component: AuthComponent, children: [
+    {path: 'login', component: LoginComponent},
+    {path: 'registration', component: RegistrationComponent}
+  ]}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule { }
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'usr-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.styl']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
