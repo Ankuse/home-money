afee0753f4bd0ba31b608a43d6d10c19b1bfe066
Ankuse <esukna@gmail.com>
2017-12-01 15:18:14 +0300

    update package json change true command for dev command -- correctly npm dev --- End rgistration and login component and module ---- Create shared module , take FormsModule and ReactiveFormsModule. Validation for form complete (email and password ) ----- Add userService and add login and correct auth, add path to registration ------ Add new method (createNewUser) in userService, add registration system ------- Create registration and login . Full

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -12,7 +12,7 @@ import {Router} from '@angular/router';
 export class RegistrationComponent implements OnInit {
 
   form: FormGroup;
-  nowCanLogin;
+
   constructor (
       private usersService: UsersService,
       private router: Router
@@ -21,12 +21,13 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit () {
     this.form = new FormGroup({
-      'email' : new FormControl(null, [Validators.required, Validators.email]),
+      'email' : new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
       'password' : new FormControl(null, [Validators.required, Validators.minLength(6)]),
       'name' : new FormControl(null, [Validators.required]),
       'agree' : new FormControl(false, [Validators.requiredTrue]),
     });
   }
+
   onSubmit () {
     const {email, password, name} = this.form.value;
     const user = new User(email, password, name);
@@ -38,6 +39,29 @@ export class RegistrationComponent implements OnInit {
             }
           });
           console.log(user);
+
+          window.setTimeout( () => {
+            this.usersService.createNewUser( user )
+                .subscribe( () => {
+                  this.router.navigate(['/login'] );
+                });
+          }, 2000);
+
         } );
   }
+
+  forbiddenEmails (control: FormControl): Promise<any> {
+    return new Promise( (resolve, reject) => {
+      this.usersService.getUserByEmail(control.value).subscribe( (user: User) => {
+        if (user) {
+          resolve({
+            forbiddenEmail: true
+          });
+        } else {
+          resolve(null);
+        }
+      });
+    } );
+  }
+
 }
